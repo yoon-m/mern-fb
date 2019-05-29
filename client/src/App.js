96f62653fe05e637e81eb167f2b29907d5d4acd2
yoon-m <myoon1098@gmail.com>
2019-05-29 19:17:58 -0400

    initialize redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import { Switch, HashRouter, Route } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store from './store/store';
+
 import Login from './components/auth/Login';
 
 class App extends React.Component {
   render() {
     return (
-      <HashRouter>
-        <Switch>
-          <Route path='/login' component={Login} />
-        </Switch>
-      </HashRouter>
+      <Provider store={store} >
+        <HashRouter>
+          <Switch>
+            <Route path='/login' component={Login} />
+          </Switch>
+        </HashRouter>
+      </Provider>
     )
   }
 }
