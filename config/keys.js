55f18c956dc65dade40bb1b31ace3ac332794e4f
yoon-m <myoon1098@gmail.com>
2019-05-29 14:37:37 -0400

    initial server setup

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://dbAdmin:4N524oka!@cluster0-pu55f.mongodb.net/test?retryWrites=true"
+};
--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require('express');
-require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(
+    db, { useNewUrlParser: true }
+).then(() => console.log("MongoDB successfully connected"))
+.catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send("Welcome to Express");
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-});
+app.listen(port, () => console.log(`Server running on port: ${port}.`));
