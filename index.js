7e7438010500508f8277b2ce2ada82d9dd6c7a61
yoon-m <myoon1098@gmail.com>
2019-05-29 15:26:41 -0400

    functional login/register

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
 
@@ -17,6 +20,11 @@ mongoose.connect(
     db, { useNewUrlParser: true }
 ).then(() => console.log("MongoDB successfully connected"))
 .catch(err => console.log(err));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
